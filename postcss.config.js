cb0c23120cb0956c133482b44080a726f8f845af
Edie Lemoine <edie@myparcel.nl>
2020-10-09 21:48:43 +0200

    fix: purge tailwind styles (reduces css size from 198kb to 1.7kb)

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,22 @@
+const purgeCss = require('@fullhuman/postcss-purgecss');
+
 module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production'
+      ? [
+        purgeCss({
+          content: [
+            './public/*.html',
+            './src/**/*.html',
+            './src/**/*.js',
+            './src/**/*.vue',
+          ],
+
+          defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+        }),
+      ]
+      : [],
   ],
 };
